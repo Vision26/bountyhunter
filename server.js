9635fe2a933e308869ea245b8ce9710bf7845b07
Jose Hernandez <jhernandez_456@yahoo.com>
2022-06-10 21:34:04 -0700

    server added

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
-const app = express.Router()
+const app = express()
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.listen(4000, () => {
+    console.log('Server 4000 is up and running...')
+})
